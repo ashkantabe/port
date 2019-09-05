f5a8d84eca77c6323e77ae237c26149c6c3679b2
ashkantabe <52970831+ashkantabe@users.noreply.github.com>
2019-09-05 11:47:53 -0700

    Add files via upload

---
+++ po/js/main.js
@@ -0,0 +1,81 @@
+
+
+<script>
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Delete line 12, close the comment on line 7");
+console.log("The images shlould be inside the image folder");
+console.log("the Path of the images are correct? the name of the image is corret too?")
+console.log("I Think your Css is not linked properly.")
